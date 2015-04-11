a407c44578f17a5262e06bfbfc4fb22a4219d05e
Raffique <Raffique@users.noreply.github.com>
2015-04-11 00:43:22 -0500

    Create pixy cam code.ino

---
+++ pixy cam code.ino
@@ -0,0 +1,41 @@
+
+void setup()
+{
+
+  Serial.begin(9600);
+  Serial.print("Starting...\n");
+
+  pixy.init();
+}
+
+void loop()
+{ 
+  static int i = 0;
+  int j;
+  uint16_t blocks;
+  char buf[32]; 
+  
+  blocks = pixy.getBlocks();
+  
+  if (blocks)
+  {
+    i++;
+    
+    if (i%50==0)
+    {
+      sprintf(buf, "Detected %d:\n", blocks);
+      Serial.print(buf);
+      for (j=0; j<blocks; j++)
+      {
+        sprintf(buf, "  block %d: ", j);
+        Serial.print(buf); 
+        pixy.blocks[j].print();
+        Serial.println(pixy.blocks[i].signature);
+        Serial.println(pixy.blocks[i].x);
+        Serial.println(pixy.blocks[i].y);
+        
+        
+      }
+    }
+  }  
+}
